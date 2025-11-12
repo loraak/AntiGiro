0195d10aa3942a086671da238d26a6217b847395
Karol Arvizu <lorakarvizu@gmail.com>
2025-11-12 13:09:52 -0600

    Detalles de build

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,4 +1,4 @@
-const API_URL = `${process.env.REACT_APP_API_URL}/`;
+const API_URL = `${process.env.REACT_APP_API_URL}`;
 
 export const apiClient = async (url, options = {}) => {
     const token = localStorage.getItem('token');
